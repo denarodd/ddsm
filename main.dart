e67a9dda77cfb25f79c8fbcc9cddce04f042a673
denaro <francacaroline197@gmail.com>
2026-04-06 14:53:26 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,37 +4,45 @@ void main() {
   runApp(MaterialApp(home: TelaInicial()));
 }
 
-class TelaInicial extends StatelessWidget {
+class TelaInicial extends StatefulWidget {
   const TelaInicial({super.key});
 
+  @override
+  State<TelaInicial> createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  List<Aluno> alunoList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.menu),
         title: Text("App da 3G"),
-        actions: [Text("Aqui tem um botão.")],
+        actions: [Text("Aqui tem butão...")],
       ),
-      body: ListView(
-        reverse: true,
-        children: [
-          Text("Ola 3"),
-          Text("Ola 3"),
-          Text("Ola 3"),
-          Text("Ola 3"),
-          Text("Ola 3"),
-          Text("Ola 3"),
-          Text("Ola 3"),
-          Text("Ola 3"),
-        ],
+      body: ListView.builder(
+        itemCount: alunoList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.person_4_rounded),
+              title: Text(alunoList[index].nome),
+              subtitle: Text(
+              "Tel.: ${alunoList[index].telefone} | Matrícula: ${alunoList[index].matricula}",
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => TelaFormulario()),
-          ).then((Aluno) {
-            debugPrint("Esse é meu Aluno: " + Aluno.Nome);
+          ).then((aluno) {
+            setState(() {
+              alunoList.add(aluno);
+            });
+            debugPrint("esse é meu aluno: " + aluno.nome);
           });
         },
         child: Icon(Icons.add),
@@ -44,32 +52,34 @@ class TelaInicial extends StatelessWidget {
 }
 
 class TelaFormulario extends StatelessWidget {
+  const TelaFormulario({super.key});
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController Nome = TextEditingController();
-    TextEditingController Telefone = TextEditingController();
-    TextEditingController Matricula = TextEditingController();
+    TextEditingController nome = TextEditingController();
+    TextEditingController telefone = TextEditingController();
+    TextEditingController matricula = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
         children: [
-          Text("Nome:"),
-          TextField(controller: Nome),
-          Text("Telefone:"),
-          TextField(controller: Telefone),
-          Text("Matricula:"),
-          TextField(controller: Matricula),
+          Text("nome:"),
+          TextField(controller: nome),
+          Text("telefone:"),
+          TextField(controller: telefone),
+          Text("matricula:"),
+          TextField(controller: matricula),
           ElevatedButton(
             onPressed: () {
-              if (Nome.text != "" &&
-                  Telefone.text != "" &&
-                  Matricula.text != "") {
-                Aluno info = Aluno(Nome.text, Telefone.text, Matricula.text);
+              if (nome.text != '' &&
+                  telefone.text != '' &&
+                  matricula.text != '') {
+                Aluno info = Aluno(nome.text, telefone.text, matricula.text);
                 Navigator.pop(context, info);
               } else {
-                debugPrint("Preencha todos os campos para continuar...");
+                debugPrint("Preencha todos os campos burrão");
               }
-              Navigator.pop(context);
             },
             child: Text("Salvar"),
           ),
@@ -80,9 +90,9 @@ class TelaFormulario extends StatelessWidget {
 }
 
 class Aluno {
-  String Nome;
-  String Telefone;
-  String Matricula;
+  String nome;
+  String telefone;
+  String matricula;
 
-  Aluno(this.Nome, this.Telefone, this.Matricula);
+  Aluno(this.nome, this.telefone, this.matricula);
 }
