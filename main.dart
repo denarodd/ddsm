57b86c07ef683589f26172fdbecc1ae3c3848daa
denaro <francacaroline197@gmail.com>
2026-04-06 13:40:30 -0300

    ola
    feliz eu e kayron

--- main.dart
+++ main.dart
@@ -13,21 +13,76 @@ class TelaInicial extends StatelessWidget {
       appBar: AppBar(
         leading: Icon(Icons.menu),
         title: Text("App da 3G"),
-        actions: [Text("Aqui tem butão...")],
+        actions: [Text("Aqui tem um botão.")],
       ),
       body: ListView(
         reverse: true,
         children: [
-          Text("Ola 5"),
-          Text("Ola 5"),
-          Text("Ola 5"),
-          Text("Ola 5"),
-          Text("Ola 5"),
-          Text("Ola 5"),
-          Text("Ola 5"),
+          Text("Ola 3"),
+          Text("Ola 3"),
+          Text("Ola 3"),
+          Text("Ola 3"),
+          Text("Ola 3"),
+          Text("Ola 3"),
+          Text("Ola 3"),
+          Text("Ola 3"),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: null),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TelaFormulario()),
+          ).then((Aluno) {
+            debugPrint("Esse é meu Aluno: " + Aluno.Nome);
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
+}
+
+class TelaFormulario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController Nome = TextEditingController();
+    TextEditingController Telefone = TextEditingController();
+    TextEditingController Matricula = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Text("Nome:"),
+          TextField(controller: Nome),
+          Text("Telefone:"),
+          TextField(controller: Telefone),
+          Text("Matricula:"),
+          TextField(controller: Matricula),
+          ElevatedButton(
+            onPressed: () {
+              if (Nome.text != "" &&
+                  Telefone.text != "" &&
+                  Matricula.text != "") {
+                Aluno info = Aluno(Nome.text, Telefone.text, Matricula.text);
+                Navigator.pop(context, info);
+              } else {
+                debugPrint("Preencha todos os campos para continuar...");
+              }
+              Navigator.pop(context);
+            },
+            child: Text("Salvar"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Aluno {
+  String Nome;
+  String Telefone;
+  String Matricula;
+
+  Aluno(this.Nome, this.Telefone, this.Matricula);
 }
